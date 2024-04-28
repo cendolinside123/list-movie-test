53f04781716ea8b66a6e5cbdcfb8a6ccef34a7eb
Jan Sebastian <>
2024-04-29 02:35:55 +0700

    add/change:
    - on progress create unit test fetch list movie

--- movieTests/MovieListTest.swift
+++ movieTests/MovieListTest.swift
@@ -11,6 +11,37 @@ import XCTest
 
 class MovieListTest: XCTestCase {
     
+    class MockupClass: MovieListDelegate {
+        
+        var error: Error?
+        var listDatalog: [[movie.MovieModel]?] = []
+        var viewModel = MovieListViewModelImpl()
+        
+        var expectation: XCTestExpectation?
+        
+        init() {
+            self.viewModel.delegate = self
+        }
+        
+        func onSuccess() {
+            listDatalog.append(viewModel.listMovie)
+            expectation?.fulfill()
+        }
+        
+        func onError(error: Error) {
+            self.error = error
+            expectation?.fulfill()
+        }
+        
+        func onLoading() {
+            
+        }
+        
+        func onEndLoading() {
+            
+        }
+    }
+    
     
     override class func setUp() {
         ServiceContainer.register(type: GenreUseCase.self, GenreUseCaseImpl())
@@ -20,7 +51,23 @@ class MovieListTest: XCTestCase {
     }
     
     func testFirstLoad() {
+        let expectation = XCTestExpectation(description: "Delegate receives events")
+        expectation.expectedFulfillmentCount = 1
         
+        let mockClass = MockupClass()
+        mockClass.expectation = expectation
+        
+        mockClass.viewModel.firstInitials()
+        
+        let result = XCTWaiter.wait(for: [expectation], timeout: 5)
+        
+        if result == XCTWaiter.Result.completed {
+            XCTAssert(mockClass.listDatalog.count == 1, "Delegate should receive one events")
+            XCTAssertNotNil(mockClass.listDatalog[0], "value should not nil")
+            XCTAssert((mockClass.listDatalog[0]?.count ?? 0) > 0, "value should bigger than zero")
+        } else {
+            XCTAssert(false, "req timeout")
+        }
     }
     
     func testFetchNewPage() {
