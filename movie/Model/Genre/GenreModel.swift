9fffa4abd608d279c1a56b3ad959fea510fc5fb1
Jan Sebastian <>
2024-04-28 22:17:46 +0700

    add/change:
    - add hashable on GenreModel

--- movie/Model/Genre/GenreModel.swift
+++ movie/Model/Genre/GenreModel.swift
@@ -9,7 +9,15 @@ import Foundation
 
 
 // MARK: - GenreModel
-struct GenreModel: Codable {
+struct GenreModel: Codable, Hashable {
     let id: Int
     let name: String
+    
+    static func == (lhs: GenreModel, rhs: GenreModel) -> Bool {
+        return lhs.id == rhs.id
+    }
+
+    func hash(into hasher: inout Hasher) {
+        
+    }
 }
