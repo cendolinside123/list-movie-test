3f6468c8935b0a68a118c43f2da38b4fbcc41ba3
Jan Sebastian <>
2024-04-29 02:50:07 +0700

    add/change:
    - create unit test display movie detail

--- movieTests/MovieDetailTest.swift
+++ movieTests/MovieDetailTest.swift
@@ -11,6 +11,38 @@ import XCTest
 
 class MovieDetailTest: XCTestCase {
     
+    class MockupClass: MovieDetailDelegate {
+        
+        var error: Error?
+        var result: (MovieDetailModel, [CastModel])?
+        var viewModel = MovieDetailViewModelImpl()
+        
+        var expectation: XCTestExpectation?
+        
+        init() {
+            viewModel.delegate = self
+        }
+        
+        func onSuccess() {
+            self.result = viewModel.movieInformation
+            expectation?.fulfill()
+        }
+        
+        func onError(error: Error) {
+            self.error = error
+            expectation?.fulfill()
+        }
+        
+        func onLoading() {
+            
+        }
+        
+        func onEndLoading() {
+            
+        }
+        
+        
+    }
     
     override class func setUp() {
         ServiceContainer.register(type: GenreUseCase.self, GenreUseCaseImpl())
@@ -20,7 +52,23 @@ class MovieDetailTest: XCTestCase {
     }
     
     func testLoadDetailmovie() {
+        let expectation = XCTestExpectation(description: "Delegate receives events")
+        expectation.expectedFulfillmentCount = 1
         
+        let mockClass = MockupClass()
+        mockClass.expectation = expectation
+        
+        mockClass.viewModel.loadMovieDetail(movieID: 7451)
+        
+        let result = XCTWaiter.wait(for: [expectation], timeout: 5)
+        
+        if result == XCTWaiter.Result.completed {
+            XCTAssertNil(mockClass.error, "error not happened")
+            XCTAssertNotNil(mockClass.result, "value movie detail not null")
+            XCTAssert((mockClass.result?.1.count ?? 0) > 0, "list cast must bigger thn zero")
+        } else {
+            XCTAssert(false, "req timeout")
+        }
     }
     
     
