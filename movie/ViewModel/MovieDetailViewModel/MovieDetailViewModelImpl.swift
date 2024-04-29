abdcd24220d5e7da7c5edcfa67b3a3ee32e08078
Jan Sebastian <>
2024-04-29 12:43:44 +0700

    add/change:
    - add process load cache movie detail and cast

--- movie/ViewModel/MovieDetailViewModel/MovieDetailViewModelImpl.swift
+++ movie/ViewModel/MovieDetailViewModel/MovieDetailViewModelImpl.swift
@@ -14,10 +14,13 @@ class MovieDetailViewModelImpl: DummyMovieDetailVM<(MovieDetailModel, [CastModel
     @Service private var useCaseMovieList: MovieUseCase
     @Service private var useCaseListCast: CastUseCase
     @Service private var usecaseMovieLocal: MovieLocalUseCase
+    @Service private var useCaseCastLocal: CastLocalUseCase
     
     override func loadMovieDetail(movieID: Int) {
         
         delegate?.onLoading()
+        let castUsecaseLocal = self.useCaseCastLocal
+        let detailMovieUscaseLocal = self.usecaseMovieLocal
         
         let loadDetail =  useCaseMovieList
             .fetchMovieDetail(movieID: movieID)
@@ -56,42 +59,72 @@ class MovieDetailViewModelImpl: DummyMovieDetailVM<(MovieDetailModel, [CastModel
                 
                 return (getDetail, castResponse.cast)
             })
+            
         
         usecaseMovieLocal
             .getMovie(movieID: movieID)
+            .observe(on: ConcurrentDispatchQueueScheduler(qos: .background))
             .flatMap({ result -> Single<(MovieDetailModel, [CastModel])> in
                 
-                if let getDetail = result?.toDetail, 
-                    let listCast = (result?.toCast?.allObjects as? [Cast]),
-                    var getDetailModel = getDetail.toNormalModel() {
-                    
-                    for item in listCast {
-                        if let getCast = item.toNormalModel() {
-                            
+                if let getResult = result  {
+                    if let getDetail = getResult.toDetail,
+                       var getDetailModel = getDetail.toNormalModel() {
+                        let listCast = (getResult.toCast?.allObjects as? [Cast])
+                        var listAllCast: [CastModel] = []
+                        
+                        for item in (listCast ?? []) {
+                            if let getCast = item.toNormalModel() {
+                                listAllCast.append(getCast)
+                            }
                         }
+                        
+                        let getSize =  NSString(string: getDetailModel.overview).boundingRect(
+                            with:
+                                CGSize(
+                                    width:
+                                        (
+                                            UIScreen.main.bounds.width - (2 * 20)
+                                        ),
+                                    height: CGFloat.greatestFiniteMagnitude),
+                            options: [.usesLineFragmentOrigin, .usesFontLeading],
+                            context: nil
+                        )
+                        
+                        
+                        let totalHeight = getSize.height + (2 * 20)
+                        
+                        getDetailModel.overViewSize = CGSize(width: getSize.width, height: totalHeight)
+                        if listAllCast.count > 0 {
+                            return Single.zip(Single.just(getDetailModel), Single.just(listAllCast))
+                        } else {
+                            let getCast = fetchCast
+                                .observe(on: ConcurrentDispatchQueueScheduler(qos: .background))
+                                .flatMap({ castResponse -> Single<[CastModel]> in
+                                    return castUsecaseLocal
+                                        .doInputCast(movie: getResult, value: castResponse.cast)
+                                        .catch({ _ in
+                                            return Completable.empty()
+                                        })
+                                        .andThen(Single.just(castResponse.cast))
+                                }).asSingle()
+                            return Single.zip(Single.just(getDetailModel), getCast)
+                        }
+                    } else {
+                        return fetchOnline
+                            .observe(on: ConcurrentDispatchQueueScheduler(qos: .background))
+                            .flatMap({ (detailMovie, listCast) -> Single<(MovieDetailModel, [CastModel])> in
+                                
+                                return detailMovieUscaseLocal
+                                    .inputMovieDetail(movie: getResult, valueDetail: detailMovie, cast: listCast)
+                                    .catch({ _ in
+                                        return Completable.empty()
+                                    }).andThen(Single.zip(Single.just(detailMovie), Single.just(listCast)))
+                            })
                     }
                     
-                    let getSize =  NSString(string: getDetailModel.overview).boundingRect(
-                        with:
-                            CGSize(
-                                width:
-                                    (
-                                        UIScreen.main.bounds.width - (2 * 20)
-                                    ),
-                                height: CGFloat.greatestFiniteMagnitude),
-                        options: [.usesLineFragmentOrigin, .usesFontLeading],
-                        context: nil
-                    )
-                    
-                    
-                    let totalHeight = getSize.height + (2 * 20)
-                    
-//                    getDetail.overViewSize = CGSize(width: getSize.width, height: totalHeight)
-//                    return Single.zip(Single.just(getDetailModel), Single.just(getCast))
+                } else {
+                    return fetchOnline
                 }
-                
-                
-                return fetchOnline
             })
             .subscribe(on: ConcurrentDispatchQueueScheduler(qos: .background))
             .observe(on: MainScheduler.instance)
