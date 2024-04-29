9501d02fc96fb8fc231d96042334affe3ed2bb56
Jan Sebastian <>
2024-04-29 11:40:18 +0700

    add/change:
    - implement cache data list movie

--- movie/ViewModel/MovieListViewModel/MovieListViewModelImpl.swift
+++ movie/ViewModel/MovieListViewModel/MovieListViewModelImpl.swift
@@ -19,6 +19,8 @@ class MovieListViewModelImpl: DummyMovielistVM<[MovieModel]> {
     @Service private var useCaseMovieList: MovieUseCase
     @Service private var useCaseLanguage: LanguageUseCase
     @Service private var useCaseGenre: GenreUseCase
+    @Service private var useCaseGenreLocal: GenreLocalUseCase
+    @Service private var usecaseMovieLocal: MovieLocalUseCase
     
     private var streamFetch: PublishSubject<(Bool, Bool, String?)> = PublishSubject()
     
@@ -89,60 +91,119 @@ extension MovieListViewModelImpl {
         let useCaseGenre = self.useCaseGenre
         let useCaseMovieList = self.useCaseMovieList
         let getCurrentPage = self.currentPage
+        let localGenreUseCase = self.useCaseGenreLocal
+        let localMovieUsecase = self.usecaseMovieLocal
+        var cacheMovie: [MovieModel] = []
         isOnLoad = true
         delegate?.onLoading()
-        useCaseLanguage
-            .doFetchLanguage()
-            .catchAndReturn([])
+        
+        // assumsion minimum genre is 19
+        localGenreUseCase
+            .fetchAllGenre()
             .observe(on: ConcurrentDispatchQueueScheduler(qos: .background))
-            .flatMap({ (result) -> Single<[GenreModel]> in
+            .flatMap( { (result) -> Single<[GenreModel]> in
                 
-                var listReqGenre: [Observable<GenreResponse>] = []
-                
-                for item in result {
+                if result.count >= 19 {
                     
-                    let fetchGenreMovie = useCaseGenre
-                        .fetchListGenreMovie(language: item.iso639_1)
-                        .asObservable()
-                        .catch({ _ in
-                            return Observable.empty()
-                        })
-                    let fetchGenreTV = useCaseGenre
-                        .fetchListGenreTV(language: item.iso639_1)
-                        .asObservable()
-                        .catch({ _ in
-                            return Observable.empty()
-                        })
-                    listReqGenre.append(fetchGenreMovie)
-                    listReqGenre.append(fetchGenreTV)
-                }
-                let doAllReq = Observable
-                    .zip(listReqGenre)
-                    .asSingle()
-                    .observe(on: ConcurrentDispatchQueueScheduler(qos: .background))
-                    .map({ (result) -> [GenreModel] in
-                        var listGenre: [GenreModel] = []
-                        
-                        for item in result {
-                            listGenre += item.genres
+                    var listGenre = [GenreModel]()
+                    
+                    for item in result {
+                        if let getGenre = item.toNormalModel() {
+                            listGenre.append(getGenre)
                         }
-                        
-                        let doFilter = Array(Set(listGenre))
-                        Constant.listStoredGenre = doFilter
-                        return doFilter
-                    })
+                    }
+                    Constant.listStoredGenre = listGenre
+                    return Single.just(listGenre)
+                } else {
+                    return useCaseLanguage
+                        .doFetchLanguage()
+                        .catchAndReturn([])
+                        .observe(on: ConcurrentDispatchQueueScheduler(qos: .background))
+                        .flatMap({ (result) -> Single<[GenreModel]> in
+                            
+                            var listReqGenre: [Observable<GenreResponse>] = []
+                            
+                            for item in result {
+                                
+                                let fetchGenreMovie = useCaseGenre
+                                    .fetchListGenreMovie(language: item.iso639_1)
+                                    .asObservable()
+                                    .catch({ _ in
+                                        return Observable.empty()
+                                    })
+                                let fetchGenreTV = useCaseGenre
+                                    .fetchListGenreTV(language: item.iso639_1)
+                                    .asObservable()
+                                    .catch({ _ in
+                                        return Observable.empty()
+                                    })
+                                listReqGenre.append(fetchGenreMovie)
+                                listReqGenre.append(fetchGenreTV)
+                            }
+                            let doAllReq = Observable
+                                .zip(listReqGenre)
+                                .asSingle()
+                                .observe(on: ConcurrentDispatchQueueScheduler(qos: .background))
+                                .map({ (result) -> [GenreModel] in
+                                    var listGenre: [GenreModel] = []
+                                    
+                                    for item in result {
+                                        listGenre += item.genres
+                                    }
+                                    
+                                    let doFilter = Array(Set(listGenre))
+                                    Constant.listStoredGenre = doFilter
+                                    return doFilter
+                                }).flatMap({ (result) -> Single<[GenreModel]> in
+                                    
+                                    var doInputGenre: [Completable] = []
+                                    
+                                    for item in result {
+                                        let doTask = localGenreUseCase
+                                            .addGenre(value: item)
+                                            .catch({ _ in
+                                                return Completable.empty()
+                                            })
+                                        doInputGenre.append(doTask)
+                                    }
+                                    
+                                    return Completable
+                                        .zip(doInputGenre)
+                                        .andThen(Single.just(result))
+                                        .subscribe(on: ConcurrentDispatchQueueScheduler(qos: .background))
+                                })
+                            
+                            return doAllReq
+                        })
+                }
+            }).observe(on: ConcurrentDispatchQueueScheduler(qos: .background))
+            .flatMap({ (_) -> Single<(MovieItemResponse, [Movie])> in
                 
-                return doAllReq
-            })
-            .observe(on: ConcurrentDispatchQueueScheduler(qos: .background))
-            .flatMap({ (_) -> Single<(MovieItemResponse)> in
+                let getCache = localMovieUsecase
+                    .getAllMovie()
+                    .observe(on: ConcurrentDispatchQueueScheduler(qos: .background))
+                    .flatMap({ listItem -> Single<[Movie]> in
+                        
+                        var listMovieModel: [MovieModel] = []
+                        
+                        for item in listItem {
+                            if let getMovie = item.toNormalModel() {
+                                listMovieModel.append(getMovie)
+                            }
+                        }
+                        cacheMovie = listMovieModel
+                        
+                        return Single.just(listItem)
+                    }).catch({ _ in
+                        return Single.just([])
+                    })
                 
                 let fetchMovieList =  useCaseMovieList
                     .fetchMovie(isAdultAllow: true, page: getCurrentPage)
-                return fetchMovieList
+                return Single.zip(fetchMovieList, getCache)
             })
             .observe(on: ConcurrentDispatchQueueScheduler(qos: .background))
-            .map({ (movieResponse) -> [MovieModel] in
+            .map({ (movieResponse, listCache) -> ([MovieModel], [Movie]) in
                 
                 var listMovie: [MovieModel] = []
                 
@@ -150,14 +211,47 @@ extension MovieListViewModelImpl {
                     listMovie.append(item.toMovieModel())
                 }
                 
-                return listMovie
+                return (listMovie, listCache)
+            })
+            .observe(on: ConcurrentDispatchQueueScheduler(qos: .background))
+            .flatMap({ (listMovie, listCache) -> Single<[MovieModel]> in
+                let onlineData = listMovie
+                
+                var listMovieModel: [MovieModel] = []
+                
+                for item in listCache {
+                    if let getMovie = item.toNormalModel() {
+                        listMovieModel.append(getMovie)
+                    }
+                }
+                
+                let cleaningLocalData = Set(listMovieModel)
+                let cleaningOnlineData = Set(onlineData)
+                
+                let getDifference = cleaningOnlineData.subtracting(cleaningLocalData)
+                
+                var listTask: [Completable] = []
+                
+                for item in listMovie {
+                    let doTask = localMovieUsecase
+                        .inputMovie(value: item)
+                        .catch({ _ in
+                            return Completable.empty()
+                        })
+                    listTask.append(doTask)
+                }
+                
+                return Completable
+                    .zip(listTask)
+                    .andThen(Single.just(onlineData))
+                    .subscribe(on: ConcurrentDispatchQueueScheduler(qos: .background))
             })
             .subscribe(on: ConcurrentDispatchQueueScheduler(qos: .background))
             .observe(on: MainScheduler.instance)
             .subscribe(onSuccess: { [weak self] result in
                 self?.currentPage += 1
                 self?.delegate?.onEndLoading()
-                if self?.listMovie == nil {
+                if self?.listMovie == nil || self?.currentPage == 1 {
                     self?.listMovie = result
                 } else {
                     self?.listMovie! += result
@@ -167,11 +261,16 @@ extension MovieListViewModelImpl {
             }, onFailure: { [weak self] error in
                 self?.delegate?.onEndLoading()
                 self?.delegate?.onError(error: error)
+                if cacheMovie.count > 0 {
+                    self?.listMovie = cacheMovie
+                    self?.delegate?.onSuccess()
+                }
             }).disposed(by: disposableBag)
     }
     
     private func doFetchMovie(isRefetch: Bool) {
         let useCaseMovieList = self.useCaseMovieList
+        let localMovieUsecase = self.usecaseMovieLocal
         
         isOnLoad = true
         
@@ -181,10 +280,16 @@ extension MovieListViewModelImpl {
         self.getPrevTask = .normalLoad
         let currentPage = self.currentPage
         delegate?.onLoading()
-        useCaseMovieList
+        let getLocalData = localMovieUsecase
+            .getAllMovie()
+            .catchAndReturn([])
+        
+        let fetchOnlineData = useCaseMovieList
             .fetchMovie(isAdultAllow: true, page: currentPage)
+        
+        Single.zip(getLocalData, fetchOnlineData)
             .observe(on: ConcurrentDispatchQueueScheduler(qos: .background))
-            .map({ (movieResponse) -> [MovieModel] in
+            .map({ (localCache, movieResponse) -> ([MovieModel], [Movie]) in
                 
                 var listMovie: [MovieModel] = []
                 
@@ -192,7 +297,39 @@ extension MovieListViewModelImpl {
                     listMovie.append(item.toMovieModel())
                 }
                 
-                return listMovie
+                return (listMovie, localCache)
+            })
+            .flatMap({ (listMovie, listCache) -> Single<[MovieModel]> in
+                let onlineData = listMovie
+                
+                var listMovieModel: [MovieModel] = []
+                
+                for item in listCache {
+                    if let getMovie = item.toNormalModel() {
+                        listMovieModel.append(getMovie)
+                    }
+                }
+                
+                let cleaningLocalData = Set(listMovieModel)
+                let cleaningOnlineData = Set(onlineData)
+                
+                let getDifference = cleaningOnlineData.subtracting(cleaningLocalData)
+                
+                var listTask: [Completable] = []
+                
+                for item in listMovie {
+                    let doTask = localMovieUsecase
+                        .inputMovie(value: item)
+                        .catch({ _ in
+                            return Completable.empty()
+                        })
+                    listTask.append(doTask)
+                }
+                
+                return Completable
+                    .zip(listTask)
+                    .andThen(Single.just(onlineData))
+                    .subscribe(on: ConcurrentDispatchQueueScheduler(qos: .background))
             })
             .observe(on: ConcurrentDispatchQueueScheduler(qos: .background))
             .map({ [weak self] (result) -> [MovieModel] in
@@ -228,6 +365,7 @@ extension MovieListViewModelImpl {
     
     private func doSearchMovie(isRefetch: Bool, keyword: String) {
         let useCaseMovieList = self.useCaseMovieList
+        let localMovieUsecase = self.usecaseMovieLocal
         
         isOnLoad = true
         
@@ -237,10 +375,17 @@ extension MovieListViewModelImpl {
         self.getPrevTask = .Search
         let currentPage = self.currentPage
         delegate?.onLoading()
-        useCaseMovieList
+        
+        let getLocalData = localMovieUsecase
+            .getAllMovie()
+            .catchAndReturn([])
+        
+        let fetchOnlineData = useCaseMovieList
             .searchMovie(keyWord: keyword, isAdultAllow: true, page: currentPage)
+        
+        Single.zip(getLocalData, fetchOnlineData)
             .observe(on: ConcurrentDispatchQueueScheduler(qos: .background))
-            .map({ (movieResponse) -> [MovieModel] in
+            .map({ (localCache, movieResponse) -> ([MovieModel], [Movie]) in
                 
                 var listMovie: [MovieModel] = []
                 
@@ -248,7 +393,39 @@ extension MovieListViewModelImpl {
                     listMovie.append(item.toMovieModel())
                 }
                 
-                return listMovie
+                return (listMovie, localCache)
+            })
+            .flatMap({ (listMovie, listCache) -> Single<[MovieModel]> in
+                let onlineData = listMovie
+                
+                var listMovieModel: [MovieModel] = []
+                
+                for item in listCache {
+                    if let getMovie = item.toNormalModel() {
+                        listMovieModel.append(getMovie)
+                    }
+                }
+                
+                let cleaningLocalData = Set(listMovieModel)
+                let cleaningOnlineData = Set(onlineData)
+                
+                let getDifference = cleaningOnlineData.subtracting(cleaningLocalData)
+                
+                var listTask: [Completable] = []
+                
+                for item in listMovie {
+                    let doTask = localMovieUsecase
+                        .inputMovie(value: item)
+                        .catch({ _ in
+                            return Completable.empty()
+                        })
+                    listTask.append(doTask)
+                }
+                
+                return Completable
+                    .zip(listTask)
+                    .andThen(Single.just(onlineData))
+                    .subscribe(on: ConcurrentDispatchQueueScheduler(qos: .background))
             })
             .observe(on: ConcurrentDispatchQueueScheduler(qos: .background))
             .map({ [weak self] (result) -> [MovieModel] in
